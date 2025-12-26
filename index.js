57e3dab72ffcd71d561394392ea15a7a3a3985cf
Dyair <danteyaiir@proton.me>
2025-12-26 11:20:34 -0600

    Fix: Configurando puerto dinámico para Render

--- index.js
+++ index.js
@@ -88,6 +88,7 @@ app.put('/productos/:id', async function (req, res) {
 
 });
 
-app.listen(3000, function() {
-    console.log('Servidor corriendo en el puerto 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () {
+    console.log(`Servidor corriendo en el puerto ${port} 🚀`);
 });
